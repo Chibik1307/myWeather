b883ab117ba84cfdaf50332d8619917c33351a23
Anna A <anna-anko@mail.ru>
2023-01-27 19:47:35 +0700

    Updates

--- assets/showHide.e0a65017.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index.8beeed25.js";const s=t=>e.exports.createElement("svg",{viewBox:"0 0 10 10",fill:"currentColor",...t},e.exports.createElement("path",{d:"M5 7L9.33013 3.25H0.669873L5 7Z"})),r="/myWeather/assets/showHide.6e7bf455.svg";export{s as ReactComponent,r as default};
